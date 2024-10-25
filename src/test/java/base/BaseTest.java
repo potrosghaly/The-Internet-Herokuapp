d859db6f2fa19f5b2c1f8b112fd2a25aefd2ae09
DELL <potrossghaly@gmail.com>
2024-10-25 16:02:46 +0300

    Level1

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,6 +1,5 @@
 package base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -18,7 +17,7 @@ public class BaseTest {
     @BeforeClass
     public void setup()
     {
-        WebDriverManager.chromedriver().setup();
+
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
         driver.manage().window().maximize();
